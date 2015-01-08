678da5cf65c62802c4db32d938f619bafce60ed4
jongrover <hello@jonathangrover.com>
2015-01-07 20:35:34 -0500

    updated gallery arrows

--- js/slider-gallery.js
+++ js/slider-gallery.js
@@ -7,10 +7,32 @@ $(function(){
   // set the width of the slide tray
   $('#slide_tray').width(slideWidth * slideCount);
 
+  //hide arrows
+  $('#next, #prev').hide();
+
+  $('#frame').hover(function() {
+    $('#next, #prev').show();
+  }, function () {
+    $('#next, #prev').hide();
+  });
+
   $('#next').click(function () {
-    pos += 1;
+    if (pos < slideCount - 1) {
+      pos += 1;
+    } else {
+      pos = 0;
+    }
     var slideAmount = slideWidth * pos;
     $('#slide_tray').animate({right: slideAmount}, 600);
   });
 
+  $('#prev').click(function () {
+    if (pos > 0) {
+      pos -= 1;
+    } else {
+      pos = slideCount - 1;
+    }
+    var slideAmount = slideWidth * pos;
+    $('#slide_tray').animate({right: slideAmount}, 600);
+  });
 });
